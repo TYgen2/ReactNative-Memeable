dde61717dfc8517613c7f255266c4f1a1885cc86
TY Chow <michaelkc0075@gmail.com>
2024-08-29 21:15:31 +0800

    fixed leaked secret

---
+++ client/Memeable/app.config.js
@@ -0,0 +1,49 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  expo: {
+    name: "Memeable",
+    slug: "Memeable",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      softwareKeyboardLayoutMode: "pan",
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "com.tychow45.memeable",
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    extra: {
+      eas: {
+        projectId: "9203708c-9d1a-4702-ab67-a806199306f8",
+      },
+    },
+    scheme: "com.tychow45.memeable",
+    plugins: [
+      [
+        "react-native-fbsdk-next",
+        {
+          appID: process.env.FACEBOOK_APP_ID,
+          displayName: "Memeable",
+          clientToken: process.env.FACEBOOK_CLIENT_TOKEN,
+          scheme: "com.tychow45.memeable",
+        },
+      ],
+    ],
+  },
+};
