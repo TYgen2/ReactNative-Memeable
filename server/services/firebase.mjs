8f70f40dbd5d8a2604a260e14da561479d9caf96
TY Chow <michaelkc0075@gmail.com>
2024-10-11 18:40:59 +0800

    removed secret file from server

--- server/services/firebase.mjs
+++ server/services/firebase.mjs
@@ -1,5 +1,8 @@
 import admin from "firebase-admin";
-import serviceAccount from "../memeable-fc76d-firebase-adminsdk-gp36d-244c2c31b2.json" assert { type: "json" };
+import dotenv from "dotenv";
+dotenv.config();
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
