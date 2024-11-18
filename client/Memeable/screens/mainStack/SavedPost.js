41aec844074294de8b22871d30005e56c29acad2
TY Chow <michaelkc0075@gmail.com>
2024-11-18 18:13:33 +0800

    added empty sticker for empty saved post page

--- client/Memeable/screens/mainStack/SavedPost.js
+++ client/Memeable/screens/mainStack/SavedPost.js
@@ -1,7 +1,8 @@
-import { StyleSheet, FlatList } from "react-native";
+import { StyleSheet, FlatList, Image, View, Text } from "react-native";
 import React, { useCallback } from "react";
 import useFetchSavedPosts from "../../hooks/fetchData/useFetchSavedPosts";
 import UserPostGrid from "../../components/post/UserPostGrid";
+import { LOADING_INDICATOR } from "../../utils/constants";
 
 const SavedPost = ({ navigation, route }) => {
   const { colors } = route.params;
@@ -15,19 +16,34 @@ const SavedPost = ({ navigation, route }) => {
     [navigation]
   );
 
+  const SavedPostEmpty = () => {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require("../../assets/empty_icon/firefly.png")}
+          style={styles.icon}
+        />
+        <Text style={styles.emptyText}>No saved posts yet</Text>
+      </View>
+    );
+  };
+
+  if (isPostsLoading) return <LOADING_INDICATOR bgColor={colors.primary} />;
+
   return (
     <FlatList
       data={savedPosts}
       renderItem={renderPost}
       numColumns={3}
       contentContainerStyle={[
-        styles.container,
+        styles.flatListcontainer,
         { backgroundColor: colors.primary },
       ]}
       overScrollMode="never"
       onEndReached={loadMorePosts}
       refreshing={isPostsLoading}
       onRefresh={refreshPosts}
+      ListEmptyComponent={SavedPostEmpty}
     />
   );
 };
@@ -35,8 +51,24 @@ const SavedPost = ({ navigation, route }) => {
 export default SavedPost;
 
 const styles = StyleSheet.create({
-  container: {
+  flatListcontainer: {
     flexGrow: 1,
     paddingBottom: 70,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  icon: {
+    width: 100,
+    height: 100,
+    opacity: 0.4,
+  },
+  emptyText: {
+    marginTop: 10,
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "grey",
   },
 });
